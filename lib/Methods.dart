74c3eb0715517cd40774001438b4ccaaa7bfee2f
Bhagyesh <bhagyesh.imscit17@gmail.com>
2022-02-16 13:54:41 +0530

    Create Account & LogIn Successfull with firebase

---
+++ lib/Methods.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chatapp/login_screen.dart';
+
+Future<User?> createAccount(String name, String email, String password) async {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  try {
+    User? user = (await _auth.createUserWithEmailAndPassword(
+            email: email, password: password))
+        .user;
+
+    if (user != null) {
+      print("Login Succesfull");
+      return user;
+    } else {
+      print("Account Creation Failed!");
+      return user;
+    }
+  } catch (e) {
+    print(e);
+    return null;
+  }
+}
+
+Future<User?> logIn(String email, String password) async{
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  try{
+    User? user = (await _auth.signInWithEmailAndPassword(
+        email: email, password: password))
+        .user;
+    if (user != null) {
+      print("Login Succesfull");
+      return user;
+    } else {
+      print("Login Failed!");
+      return user;
+    }
+  }catch (e){
+    print(e);
+    return null;
+  }
+
+}
+
+Future logOut(BuildContext context) async{
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  try{
+    await _auth.signOut().then((value){
+    Navigator.push(context, MaterialPageRoute(builder: (_) => LoginScreen()));
+    });
+  }catch(e){
+    print("Error");
+  }
+}
