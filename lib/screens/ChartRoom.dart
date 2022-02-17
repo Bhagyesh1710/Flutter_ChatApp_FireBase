3207511108cfa9207d97e7d0f23a6bc8ecd3344d
Bhagyesh <bhagyesh.imscit17@gmail.com>
2022-02-17 12:09:39 +0530

    image upload succesfull

--- lib/screens/ChartRoom.dart
+++ lib/screens/ChartRoom.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 class ChattingRoom extends StatelessWidget {
   late final Map<String, dynamic> userMap;
@@ -12,12 +17,71 @@ class ChattingRoom extends StatelessWidget {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  File? imageFile;
+
+  Future getImage() async {
+    ImagePicker _picker = ImagePicker();
+
+    await _picker.pickImage(source: ImageSource.gallery).then((xFile) {
+      if(xFile != null){
+        imageFile = File(xFile.path);
+        uploadImage();
+      }
+    });
+  }
+
+  Future uploadImage() async{
+
+    String fileName = Uuid().v1();
+    int status = 1;
+
+    await _firestore
+        .collection('chatroom')
+        .doc(chatRoomId)
+        .collection('chats')
+        .doc(fileName)
+        .set({
+      "sendby": _auth.currentUser!.displayName,
+      "message": "",
+      "type": "img",
+      "time": FieldValue.serverTimestamp(),
+    });
+
+    var ref = FirebaseStorage.instance.ref().child('images').child("$fileName.jpg");
+
+    var uploadTask = await ref.putFile(imageFile!).catchError((error) async {
+      await _firestore
+          .collection('chatroom')
+          .doc(chatRoomId)
+          .collection('chats')
+          .doc(fileName)
+          .delete();
+
+      status = 0;
+    });
+
+    if (status == 1) {
+      String imageUrl = await uploadTask.ref.getDownloadURL();
+
+      await _firestore
+          .collection('chatroom')
+          .doc(chatRoomId)
+          .collection('chats')
+          .doc(fileName)
+          .update({"message": imageUrl});
+
+      print(imageUrl);
+    }
+
+
+  }
+
   void onSendMessage() async{
     if(_message.text.isNotEmpty){
       Map<String,dynamic> messages = {
         "sendby": _auth.currentUser!.displayName,
         "message": _message.text,
-        //"type": "text",
+        "type": "text",
         "time": FieldValue.serverTimestamp(),
       };
 
@@ -40,7 +104,26 @@ class ChattingRoom extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text(userMap['name']),
+        title: StreamBuilder<DocumentSnapshot>(
+          stream:  _firestore.collection("users").doc(userMap['uid']).snapshots(),
+          builder: (context,snapshot){
+            if (snapshot.data != null) {
+              return Container(
+                child: Column(
+                  children: [
+                    Text(userMap['name']),
+                    Text(
+                      snapshot.data!['status'],
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return Container();
+            }
+          },
+        ),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -63,7 +146,7 @@ class ChattingRoom extends StatelessWidget {
                         itemBuilder: (context, index) {
                           Map<String, dynamic> map = snapshot.data!.docs[index]
                               .data() as Map<String, dynamic>;
-                         return messages(size,map);
+                         return messages(size,map,context);
                         },
                       );
                     } else {
@@ -85,14 +168,17 @@ class ChattingRoom extends StatelessWidget {
                       width: size.width / 1.3,
                       child: TextField(
                         controller: _message,
+
                         decoration: InputDecoration(
+                          hintText: "Send Message",
+                          suffixIcon: IconButton(onPressed: () => getImage(), icon: Icon(Icons.photo)),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8),
                           ),
                         ),
                       ),
                     ),
-                    IconButton(icon: Icon(Icons.send), onPressed: onSendMessage),
+                    IconButton(icon: Icon(Icons.send_rounded), onPressed: onSendMessage),
                   ],
                 ),
               ),
@@ -102,9 +188,9 @@ class ChattingRoom extends StatelessWidget {
       ),
     );
   }
-  Widget messages(Size size,Map<String, dynamic> map) {
+  Widget messages(Size size,Map<String, dynamic> map,BuildContext context) {
 
-    return Container(
+    return map['type'] == "text" ? Container(
       width: size.width,
       alignment: map['sendby'] == _auth.currentUser!.displayName
           ? Alignment.centerRight
@@ -126,6 +212,54 @@ class ChattingRoom extends StatelessWidget {
         ),
       ),
 
+    ) : Container(
+      height: size.height / 2.5,
+      width: size.width,
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+      alignment: map['sendby'] == _auth.currentUser!.displayName
+          ? Alignment.centerRight
+          : Alignment.centerLeft,
+      child: InkWell(
+        onTap: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => ShowImage(
+              imageUrl: map['message'],
+            ),
+          ),
+        ),
+        child: Container(
+          height: size.height / 2.5,
+          width: size.width / 2,
+          decoration: BoxDecoration(border: Border.all()),
+          alignment: map['message'] != "" ? null : Alignment.center,
+          child: map['message'] != ""
+              ? Image.network(
+            map['message'],
+            fit: BoxFit.cover,
+          )
+              : CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
+
+class ShowImage extends StatelessWidget {
+  final String imageUrl;
+
+  ShowImage({required this.imageUrl, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        color: Colors.black,
+        child: Image.network(imageUrl),
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,12 +11,37 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver{
+
   Map<String, dynamic>? userMap;
   bool isLoading = false;
   final TextEditingController _search = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  void initState(){
+    super.initState();
+    WidgetsBinding.instance!.addObserver(this);
+    setStatus("Online");
+  }
+
+  void setStatus(String status) async {
+    await _firestore.collection('users').doc(_auth.currentUser!.uid).update({
+      "status": status,
+    });
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state){
+    if (state == AppLifecycleState.resumed) {
+      // online
+      setStatus("Online");
+    } else {
+      // offline
+      setStatus("Offline");
+    }
+  }
 
 
   String chatRoomId(String user1, String user2) {
